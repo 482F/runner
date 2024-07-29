6aa4ad94d951680fe9ce12ae6921daf98775e858
482F <482F.git.mail@482f.net>
2024-07-30 00:00:00 +0900

    hidden: true のときにのみウィンドウを隠すように

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::process::Command;
 #[derive(Deserialize)]
 struct Setting {
     cmd: String,
+    hidden: Option<bool>,
 }
 
 fn main() {
@@ -37,43 +38,45 @@ fn main() {
             Box::new(|body| {
                 let mut new_body = "".to_owned();
 
-                new_body.push_str(r#"
-                    # from https://stackoverflow.com/a/74976541
-                    function Hide-ConsoleWindow() {
-                      $ShowWindowAsyncCode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-                      $ShowWindowAsync = Add-Type -MemberDefinition $ShowWindowAsyncCode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+                if setting.hidden.unwrap_or(true) {
+                    new_body.push_str(r#"
+                        # from https://stackoverflow.com/a/74976541
+                        function Hide-ConsoleWindow() {
+                          $ShowWindowAsyncCode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+                          $ShowWindowAsync = Add-Type -MemberDefinition $ShowWindowAsyncCode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 
-                      $hwnd = (Get-Process -PID $pid).MainWindowHandle
-                      if ($hwnd -ne [System.IntPtr]::Zero) {
-                        # When you got HWND of the console window:
-                        # (It would appear that Windows Console Host is the default terminal application)
-                        $ShowWindowAsync::ShowWindowAsync($hwnd, 0)
-                      } else {
-                        # When you failed to get HWND of the console window:
-                        # (It would appear that Windows Terminal is the default terminal application)
+                          $hwnd = (Get-Process -PID $pid).MainWindowHandle
+                          if ($hwnd -ne [System.IntPtr]::Zero) {
+                            # When you got HWND of the console window:
+                            # (It would appear that Windows Console Host is the default terminal application)
+                            $ShowWindowAsync::ShowWindowAsync($hwnd, 0)
+                          } else {
+                            # When you failed to get HWND of the console window:
+                            # (It would appear that Windows Terminal is the default terminal application)
 
-                        # Mark the current console window with a unique string.
-                        $UniqueWindowTitle = New-Guid
-                        $Host.UI.RawUI.WindowTitle = $UniqueWindowTitle
-                        $StringBuilder = New-Object System.Text.StringBuilder 1024
+                            # Mark the current console window with a unique string.
+                            $UniqueWindowTitle = New-Guid
+                            $Host.UI.RawUI.WindowTitle = $UniqueWindowTitle
+                            $StringBuilder = New-Object System.Text.StringBuilder 1024
 
-                        # Search the process that has the window title generated above.
-                        $TerminalProcess = (Get-Process | Where-Object { $_.MainWindowTitle -eq $UniqueWindowTitle })
-                        # Get the window handle of the terminal process.
-                        # Note that GetConsoleWindow() in Win32 API returns the HWND of
-                        # powershell.exe itself rather than the terminal process.
-                        # When you call ShowWindowAsync(HWND, 0) with the HWND from GetConsoleWindow(),
-                        # the Windows Terminal window will be just minimized rather than hidden.
-                        $hwnd = $TerminalProcess.MainWindowHandle
-                        if ($hwnd -ne [System.IntPtr]::Zero) {
-                          $ShowWindowAsync::ShowWindowAsync($hwnd, 0)
-                        } else {
-                          Write-Host "Failed to hide the console window."
+                            # Search the process that has the window title generated above.
+                            $TerminalProcess = (Get-Process | Where-Object { $_.MainWindowTitle -eq $UniqueWindowTitle })
+                            # Get the window handle of the terminal process.
+                            # Note that GetConsoleWindow() in Win32 API returns the HWND of
+                            # powershell.exe itself rather than the terminal process.
+                            # When you call ShowWindowAsync(HWND, 0) with the HWND from GetConsoleWindow(),
+                            # the Windows Terminal window will be just minimized rather than hidden.
+                            $hwnd = $TerminalProcess.MainWindowHandle
+                            if ($hwnd -ne [System.IntPtr]::Zero) {
+                              $ShowWindowAsync::ShowWindowAsync($hwnd, 0)
+                            } else {
+                              Write-Host "Failed to hide the console window."
+                            }
+                          }
                         }
-                      }
-                    }
-                    Hide-ConsoleWindow
-                "#);
+                        Hide-ConsoleWindow
+                    "#);
+                }
 
                 new_body.push_str(body);
                 new_body
